27f213032776682374d6ed20c5c97276e3832dfd
Laecio Dev <laecio12silva@gmail.com>
2022-02-26 18:07:53 -0300

    Criando componete ProgressBar

--- src/components/Timer/styles.ts
+++ src/components/Timer/styles.ts
@@ -89,25 +89,3 @@ export const Separator = styled.span `
   
 `;
 
-export const ProgressBarContainer = styled.section `
-  display: flex;
-  flex-direction: column;
-
-  img {
-    margin-top: 10px;
-    margin-bottom: 10px;
-  }
-
-  button {
-    border: none;
-    border-radius: 0.25rem;
-    color: white;
-    margin-top: 1rem;
-    padding: 0.7rem;
-  }
-
-  button:hover {
-    transition: 0ms.2s;
-    filter: brightness(0.8);
-  }
-`;
